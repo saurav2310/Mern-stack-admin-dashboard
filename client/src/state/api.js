64e843fb44dca65356134604dd7fceb56581cbee
saurav2310 <59723908+saurav2310@users.noreply.github.com>
2023-07-26 18:46:07 +0530

    Update api.js

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: /*process.env.REACT_APP_BASE_URL*/"https://mern-stack-admin-dashboard-eight.vercel.app/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
   tagTypes: ["User", "Products", "Customers", "Transactions", "Geography","Sales","Admins","Performance","Dashboard"],
   endpoints: (build) => ({
